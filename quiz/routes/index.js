ce1d81b641002797dc81522b4bf5152f10713dfd
gilpanal <gilpanal@gmail.com>
2015-07-06 23:14:46 +0200

    Primera pregunta

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+var quizController = require('../controllers/quiz_controller');
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
 module.exports = router;
