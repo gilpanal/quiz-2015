a096c4ac9a35c3bc90ed759ccf559995ae1e7093
gilpanal <gilpanal@gmail.com>
2015-07-11 11:41:44 +0200

    No incluye autores

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -6,9 +6,6 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/author', function(req, res) {
-  res.render('author', { author: 'José Manuel Gil Panal' });
-});
 var quizController = require('../controllers/quiz_controller');
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer', quizController.answer);
