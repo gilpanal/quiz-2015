0d54d518fad4cbda13213e9fd9e12f54f8827588
gilpanal <gilpanal@gmail.com>
2015-07-23 20:11:22 +0200

    Autoload

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
@@ -9,9 +11,14 @@ router.get('/', function(req, res) {
 router.get('/author', function(req, res) {
   res.render('author', { author: 'José Manuel Gil Panal' });
 });
-var quizController = require('../controllers/quiz_controller');
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
 //router.get('/quizes/question',quizController.question);
 //router.get('/quizes/answer', quizController.answer);
+
+// Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
