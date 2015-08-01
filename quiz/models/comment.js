34a6d6199ac86b354fd84a489d88601e5bd295de
gilpanal <gilpanal@gmail.com>
2015-08-02 00:15:30 +0200

    Corregir Autores y comentario en blanco

--- quiz/models/comment.js
+++ quiz/models/comment.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes){
 			
 			texto:{
 				type:DataTypes.STRING,
-				validate:{notEmpty:{msg:"-> Falta pregunta"}}
+				validate:{notEmpty:{msg:"-> Falta comentario"}}
 			}
 		}
 	);
--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -3,32 +3,41 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
-/* GET home page. */
+// Pagina de entrada home page
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors:[] });
 });
 
 router.get('/author', function(req, res) {
-  res.render('author', { author: 'José Manuel Gil Panal' });
+  res.render('author', { author: 'José Manuel Gil Panal', errors:[] });
 });
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 
+// Definicion de rutas de sesion
+router.get('/login',sessionController.new); // formulario login
+router.post('/login', sessionController.create); // crear sesion
+router.get('/logout', sessionController.destroy); // destruir sesion
+
+
 //router.get('/quizes/question',quizController.question);
 //router.get('/quizes/answer', quizController.answer);
 
 // Definicion de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)',quizController.destroy);
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
 
+router.get('/quizes/new', 					sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', 				sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizController.destroy);
+
+// Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
