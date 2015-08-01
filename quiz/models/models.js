75f03e2e2b1b5d2803b18b7bd577358e30f124bf
gilpanal <gilpanal@gmail.com>
2015-08-01 20:53:35 +0200

    Añadir comentarios

--- quiz/models/models.js
+++ quiz/models/models.js
@@ -31,6 +31,13 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+// Importar definicion de la tabla Quiz
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 // Usar BBDD SQLite
 //var sequelize = new Sequelize(null, null, null, {dialect:"sqlite", storage:"quiz.sqlite"});
 
@@ -38,6 +45,7 @@ var Quiz = sequelize.import(quiz_path);
 //var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
